8911223144c02d4c261d6a3f26a7d5cbff683a2f
bitdeep <19378023+bitdeep@users.noreply.github.com>
2021-11-30 10:48:07 -0300

    sample that fetch wallet balances.

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-Fetch user balance from terra blockchain
+# Fetch user balance 
+
+This is sample code to demonstrate how to fetch balance from a wallet on Terra chain.
+
+# To Investigate
+
+To fetch balances from a specific contract like a farm, we need to query contract, sample code:
+`await lcdClient.wasm.contractQuery(contractAddress, { balance: { address: your_wallet_address } })`
--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,27 @@
-import { LCDClient } from '@terra-money/terra.js';
+import {Coins, LCDClient} from '@terra-money/terra.js';
 
 const terra = new LCDClient({
     URL: 'https://lcd.terra.dev',
     chainID: 'columbus-5'
 });
 
-async function fetchMarketData(){
-    const marketParams = await terra.market.parameters();
-    const exchangeRates = await terra.oracle.exchangeRates();
-    console.log(marketParams.base_pool);
-    console.log(exchangeRates.get('uusd'));
+async function fetchTokens(){
+    const accountInfo = await terra.auth.accountInfo(
+        'terra1zsky63r58vc7dfn3ljj32ch6fyn4e5qd8skzyz'
+    );
+    // console.log(accountInfo);
+    const r = await terra.bank.balance('terra1zsky63r58vc7dfn3ljj32ch6fyn4e5qd8skzyz');
+    const coins:Coins = r[0];
+    console.log(coins);
 }
 
 async function main() {
-    await fetchMarketData();
+    try{
+        await fetchTokens();
+    }catch(e){
+        console.log(e);
+    }
+
 }
 
-main();
+    main();
